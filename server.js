1e4f97ec9157aa89105f10c5e29043c7b4875d12
vidya2sri112 <vidyasri021@gmail.com>
2025-08-18 17:20:43 +0530

    Added MongoDB Atlas connection

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/studyplanner', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
